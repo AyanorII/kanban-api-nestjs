b0b7e28649e30ce13193a7e85f9674b90242e9e4
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-11 15:48:49 +0900

    added Board resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
