ff336ce12ab9a4fdfb41ee51319f21f4a4232f70
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-12 21:32:37 +0900

    add Column entity and add Board relationship

--- src/boards/entities/board.entity.ts
+++ src/boards/entities/board.entity.ts
@@ -1,10 +1,20 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column as TableColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Column } from '../../columns/entities/column.entity';
 
 @Entity()
 export class Board extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @TableColumn()
   name: string;
+
+  @OneToMany(() => Column, (column) => column.board)
+  columns: Column[];
 }
--- src/columns/entities/column.entity.ts
+++ src/columns/entities/column.entity.ts
@@ -1,1 +1,22 @@
-export class Column {}
+import { Board } from 'src/boards/entities/board.entity';
+import {
+  BaseEntity,
+  Column as TableColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Column extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @TableColumn()
+  name: string;
+
+  @ManyToOne(() => Board, (board) => board.columns, {
+    onDelete: 'CASCADE',
+  })
+  board: Board;
+}
