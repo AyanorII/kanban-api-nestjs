03627f2f60905675b9e8f101e81a26a5857bd369
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-15 10:57:04 +0900

    import ColumnsModule on TasksModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TasksService } from './tasks.service';
+import { ColumnsModule } from '../columns/columns.module';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [ColumnsModule],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
 })
 export class TasksModule {}
