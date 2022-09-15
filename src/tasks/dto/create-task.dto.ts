014c033240b7f0792ea8b04216273e9b36780339
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-15 10:29:15 +0900

    add CreateTaskDto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTaskDto {}
+import { IsString } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateTaskDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  status: string;
+
+  @ApiProperty()
+  @IsInt()
+  @IsNotEmpty()
+  columnId: number;
+}
