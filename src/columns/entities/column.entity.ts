336d8a0b09d08a54636efde9424de22fb7d87b0a
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-15 10:16:51 +0900

    add task entity and column relationship

--- src/columns/entities/column.entity.ts
+++ src/columns/entities/column.entity.ts
@@ -1,9 +1,11 @@
 import { Board } from 'src/boards/entities/board.entity';
+import { Task } from 'src/tasks/entities/task.entity';
 import {
   BaseEntity,
   Column as TableColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -19,4 +21,9 @@ export class Column extends BaseEntity {
     onDelete: 'CASCADE',
   })
   board: Board;
+
+  @OneToMany(() => Task, (task) => task.column, {
+    onDelete: 'CASCADE',
+  })
+  tasks: Task[];
 }
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,28 @@
-export class Task {}
+import { Column } from 'src/columns/entities/column.entity';
+import {
+  BaseEntity,
+  Column as TableColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Task extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @TableColumn()
+  title: string;
+
+  @TableColumn()
+  description: string;
+
+  @TableColumn()
+  status: string;
+
+  @ManyToOne(() => Column, (column) => column.tasks, {
+    onDelete: 'CASCADE',
+  })
+  column: Column;
+}
