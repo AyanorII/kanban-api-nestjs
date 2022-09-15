d36fc386ff3ac73b75852cd73d2ccfc0ff62b598
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-15 10:36:51 +0900

    add task findOne

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -24,8 +24,14 @@ export class TasksService {
     return Task.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async findOne(id: number): Promise<Task> {
+    const task = await Task.findOne({ where: { id } });
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID: '${id}' not found.`);
+    }
+
+    return task;
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
