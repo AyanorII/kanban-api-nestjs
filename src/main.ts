0ddaadfb3de104b2f14e7430515dc4001f436833
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-15 10:31:30 +0900

    remove EntityNotFoundException import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { EntityNotFoundExceptionFilter } from './entity-not-found-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
