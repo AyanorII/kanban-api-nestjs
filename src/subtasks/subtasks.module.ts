99dbdb0c68a52547e7a731de82c191fa47bbd329
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-16 11:52:41 +0900

    export subtasksService

--- src/subtasks/subtasks.module.ts
+++ src/subtasks/subtasks.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ColumnsModule } from '../columns/columns.module';
+import { TasksModule } from '../tasks/tasks.module';
+import { TasksService } from '../tasks/tasks.service';
 import { SubtasksController } from './subtasks.controller';
 import { SubtasksService } from './subtasks.service';
-import { TasksService } from '../tasks/tasks.service';
-import { TasksModule } from '../tasks/tasks.module';
-import { ColumnsModule } from '../columns/columns.module';
 
 @Module({
   imports: [TasksModule, ColumnsModule],
   controllers: [SubtasksController],
   providers: [SubtasksService, TasksService],
+  exports: [SubtasksService],
 })
 export class SubtasksModule {}
