3507a00535408de5db299fdd2b6a8d5c09f45690
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-20 18:22:20 +0900

    add NODE_ENV and rename env variables

--- config.schema.ts
+++ config.schema.ts
@@ -1,11 +1,12 @@
 import * as Joi from '@hapi/joi';
 
 export const configSchemaValidation = Joi.object({
-  PGHOST: Joi.string().required(),
-  PGPORT: Joi.number().default(5432).required(),
-  PGUSER: Joi.string().required(),
-  PGPASSWORD: Joi.string().required(),
-  PGDATABASE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
   PORT: Joi.number().required(),
   API_URL: Joi.string().required(),
+  NODE_ENV: Joi.string().required(),
 });
--- ormconfig.ts
+++ ormconfig.ts
@@ -1,19 +1,20 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { BaseEntity, DataSource } from 'typeorm';
 
 export const ormConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
   useFactory: async (configService: ConfigService) => ({
     type: 'postgres',
-    host: configService.get('PGHOST'),
-    port: configService.get('PGPORT'),
-    username: configService.get('PGUSER'),
-    password: configService.get('PGPASSWORD'),
-    database: configService.get('PGDATABASE'),
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USER'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}', BaseEntity],
     migrations: [__dirname + '/migrations/*{.ts,.js}'],
-    synchronize: true,
+    synchronize: configService.get('NODE_ENV') === 'development',
     autoLoadEntities: true,
   }),
 };
