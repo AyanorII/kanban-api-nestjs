320efb281867b638a39b888d9aaff41cf7fbfe4b
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-21 09:20:46 +0900

    add prisma schema

--- config.schema.ts
+++ config.schema.ts
@@ -9,4 +9,5 @@ export const configSchemaValidation = Joi.object({
   PORT: Joi.number().required(),
   API_URL: Joi.string().required(),
   NODE_ENV: Joi.string().required(),
+  DATABASE_URL: Joi.string().required(),
 });
