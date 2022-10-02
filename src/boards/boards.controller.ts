547eb59ef25f2afbfdfc0407e33d287f554a2087
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-02 20:53:53 +0900

    protect /boards route

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -7,35 +7,45 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Board, Column } from '@prisma/client';
+import { Board, Column, User } from '@prisma/client';
+import { GetUser } from 'src/get-user.decorator';
 // import { ColumnsService } from '../columns/columns.service';
+import { AuthGuard } from '@nestjs/passport';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 
 @ApiTags('Boards')
 @Controller('boards')
+@UseGuards(AuthGuard('jwt'))
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Post()
-  async create(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+  async create(
+    @Body() createBoardDto: CreateBoardDto,
+    @GetUser() user: User,
+  ): Promise<Board> {
     const board = await this.boardsService.create(createBoardDto);
 
     return board;
   }
 
   @Get()
-  async findAll(): Promise<Board[]> {
+  async findAll(@GetUser() user: User): Promise<Board[]> {
     const boards = await this.boardsService.findAll();
 
     return boards;
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: number): Promise<Board> {
+  async findOne(
+    @Param('id') id: number,
+    @GetUser() user: User,
+  ): Promise<Board> {
     return this.boardsService.findOne(id);
   }
 
@@ -43,6 +53,7 @@ export class BoardsController {
   async update(
     @Param('id') id: number,
     @Body() updateBoardDto: UpdateBoardDto,
+    @GetUser() user: User,
   ): Promise<Board> {
     return this.boardsService.update(id, updateBoardDto);
   }
@@ -54,7 +65,10 @@ export class BoardsController {
   }
 
   @Get(':id/columns')
-  async findBoardColumns(@Param('id') id: number): Promise<Column[]> {
+  async findBoardColumns(
+    @Param('id') id: number,
+    @GetUser() user: User,
+  ): Promise<Column[]> {
     return this.boardsService.findBoardColumns(id);
   }
 }
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,8 +3,10 @@ import { ColumnsService } from '../columns/columns.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService, ColumnsService, PrismaService],
   exports: [BoardsService],
