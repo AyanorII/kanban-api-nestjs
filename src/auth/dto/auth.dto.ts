9a3dcb21d21cdd00ea74903c3e89ec46c02e0dc2
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-05 11:00:24 +0900

    add  Matches decorator to password dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsString, Matches } from 'class-validator';
 
 @Injectable()
 export class AuthDto {
@@ -9,5 +9,7 @@ export class AuthDto {
   email: string;
 
   @ApiProperty()
+  @IsString()
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
   password: string;
 }
