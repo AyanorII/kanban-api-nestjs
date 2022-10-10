a1bcef913fe15933df8bc3172002f535df80ffb4
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-10 14:48:35 +0900

    add google strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,21 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { SeedModule } from 'src/seed/seed.module';
 import { BoardsModule } from '../boards/boards.module';
+import { ColumnsModule } from '../columns/columns.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { SeedService } from '../seed/seed.service';
+import { SubtasksModule } from '../subtasks/subtasks.module';
+import { TasksModule } from '../tasks/tasks.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { GoogleStrategy } from './google.strategy';
 import { JwtStrategy } from './jwt.strategy';
-import { ColumnsModule } from '../columns/columns.module';
-import { TasksModule } from '../tasks/tasks.module';
-import { SubtasksModule } from '../subtasks/subtasks.module';
 @Module({
   imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PassportModule.register({ defaultStrategy: ['jwt', 'google'] }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -34,9 +35,10 @@ import { SubtasksModule } from '../subtasks/subtasks.module';
     AuthService,
     PrismaService,
     JwtStrategy,
+    GoogleStrategy,
     ConfigService,
     SeedService,
   ],
-  exports: [JwtStrategy, PassportModule],
+  exports: [JwtStrategy, PassportModule, GoogleStrategy],
 })
 export class AuthModule {}
---
+++ src/auth/google.strategy.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-google-oauth2';
+import { VerifyCallback } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(@Inject(ConfigService) readonly configService: ConfigService) {
+    super({
+      clientID: configService.get('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
+      callbackURL: 'http://localhost:8081/auth/google/redirect',
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
