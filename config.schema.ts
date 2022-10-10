5a26845e3e59ba266c1a8e548b175450125a5dba
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-10 14:47:28 +0900

    refactor config schema and add google auth env variables

--- config.schema.ts
+++ config.schema.ts
@@ -1,15 +1,12 @@
 import * as Joi from '@hapi/joi';
 
 export const configSchemaValidation = Joi.object({
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().default(5432).required(),
-  DB_USER: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  DB_DATABASE: Joi.string().required(),
   PORT: Joi.number().required(),
   API_URL: Joi.string().required(),
   NODE_ENV: Joi.string().required(),
   DATABASE_URL: Joi.string().required(),
   JWT_SECRET: Joi.string().required().min(64),
   CORS_ORIGIN: Joi.string().required(),
+  GOOGLE_CLIENT_ID: Joi.string().required(),
+  GOOGLE_CLIENT_SECRET: Joi.string().required(),
 });
